ea3487a66aeb5206a801b8c4fb4e9a5f4cc529f6
Ramiro <r.gg.3@outlook.com>
2023-05-31 22:19:02 -0600

    eliminando el proyecto anterior este es otro pedo

--- README.md
+++
@@ -1,4 +0,0 @@
-# TIPS
-
-## CashApp - $love4sur3
-## Maza - $ramiro.garcia_gonzalez
--- files/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo Prueba::_header();
-    echo Prueba::_title();
-    echo "DOCUMENTANDO MI C&Oacute;DIGO.";
-    echo rg('h1');
-echo rg('header');
